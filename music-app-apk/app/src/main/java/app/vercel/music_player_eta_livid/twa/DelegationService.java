4cc48e5521a2bd8697c7c0cc00335764bd97c9b4
Narayanasriman <sriman.nexevo@gmail.com>
2026-02-05 07:25:09 +0530

    icon mismatch

---
+++ music-app-apk/app/src/main/java/app/vercel/music_player_eta_livid/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.music_player_eta_livid.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
